1e6e0fd7c28739c05ba7bb87bf261c754c0b097c
fianine <princealfiyana@gmail.com>
2018-11-23 17:16:21 +0700

    Updates

---
+++ precache-manifest.34897ee9a3c84be5de8112ba7286dfd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fianine-web/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ab830497ae4411f41b48",
+    "url": "/fianine-web/static/js/runtime~main.ab830497.js"
+  },
+  {
+    "revision": "e75e7caf8a078fb4b090",
+    "url": "/fianine-web/static/js/main.e75e7caf.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/fianine-web/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "e75e7caf8a078fb4b090",
+    "url": "/fianine-web/static/css/main.3a13b82f.chunk.css"
+  },
+  {
+    "revision": "6628a2ea8eb8ee36a2d23df2c0767de9",
+    "url": "/fianine-web/index.html"
+  }
+];
